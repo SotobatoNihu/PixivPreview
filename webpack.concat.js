d190422a6fac0b16b1d9e517ed834f4c3182bef2
SotobatoNihu <33326634+SotobatoNihu@users.noreply.github.com>
2018-09-16 14:44:15 +0900

    Update webpack.concat.js
    不要なコメントを削除

--- webpack.concat.js
+++ webpack.concat.js
@@ -2,11 +2,6 @@ const ConcatPlugin = require('webpack-concat-plugin');
 
 module.exports = {
     entry: './dist/bundle.js',
-    /*
-    output: {
-        filename: 'result.js'
-    },
-    */
     plugins: [
         new ConcatPlugin({
             uglify: false,
@@ -18,44 +13,3 @@ module.exports = {
     ],
     // ・・・
 }
-//const WrapperPlugin = require('wrapper-webpack-plugin');
-//const fs = require('fs');
-//const headerDoc = require('./src/header.txt', 'utf8');
-//const footerDoc = require('./src/footer.txt', 'utf8');
-
-//const webpack = require('webpack');
-//const fs = require('fs');
-//const headerDoc = fs.readFileSync('./src/header.js', 'utf8');
-/*
-module.exports = {
-   entry: './src/main.js',
-   output: {
-       path: '/dist',
-       filename: 'bundle.js',
-   },
-   plugins: [
-       new UglifyEsPlugin({
-           // beautify: {},
-       }),
-
-       new ConcatPlugin({
-           uglify: false,
-           sourceMap: false,
-           name: 'result',
-           fileName: '[name].js',
-           filesToConcat: ['src/header.txt', 'dist/bundle.js', 'src/footer.txt'],
-       }),
-       */
-
-    /*
-    new UglifyEsPlugin({
-           // beautify: {},
-    }),
-    new webpack.BannerPlugin({
-        test: /\.js$/,
-        banner: headerDoc,
-    })
-
-};
-
- */
